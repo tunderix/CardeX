0ff414370885993c57a1f506dbf7fb272fd98d2c
Ioni <sami.repo@live.com>
2016-08-22 18:11:20 +0300

    Refactored base of server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
-module.exports = {
-  Server: require('./lib/server'),
-  Room: require('./lib/room'),
-  utils: require('./lib/utils'),
-  protocol: require('./lib/protocol')
-}
+"use strict";
+
+var colyseus = require('colyseus')
+  , http = require('http')
+
+  , express = require('express')
+  , app = express()
+  , port = process.env.PORT || 3553
+
+  , server = http.createServer(app)
+  , gameServer = new colyseus.Server({server: server})
+
+gameServer.register('tictactoe', require('./rooms/tictactoe'))
+server.listen(port);
+
+app.use(express.static(__dirname + "/../frontend/public"))
+
+console.log(`Listening on ws://localhost:${ port }`)
---
+++ server/rooms/tictactoe.js
@@ -0,0 +1,156 @@
+var Room = require('colyseus').Room
+  , ClockTimer = require('clock-timer.js')
+
+const PATCH_RATE = 20
+    , TURN_TIMEOUT = 10
+
+class TicTacToe extends Room {
+
+  constructor (options) {
+    // call 'update' method each 50ms
+    super(options, 1000 / PATCH_RATE)
+
+    this.setState({
+      currentTurn: null,
+      players: {},
+      board: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+      winner: null,
+      draw: null
+    })
+
+    this.clock = new ClockTimer(true)
+  }
+
+  requestJoin(options) {
+    // only 2 players are allowed to play
+    return this.clients.length < 2;
+  }
+
+  onJoin (client) {
+    client.playerIndex = Object.keys(this.state.players).length
+    this.state.players[ client.id ] = client.playerIndex
+
+    if (this.clients.length == 2) {
+      this.state.currentTurn = client.id
+      this.randomMoveTimeout = this.clock.setTimeout(this.doRandomMove.bind(this, client), TURN_TIMEOUT * 1000)
+
+      // lock this room for new users
+      this.lock()
+    }
+  }
+
+  onMessage (client, data) {
+    if (this.state.winner || this.state.draw) { return false; }
+
+    if (client.id === this.state.currentTurn) {
+      if (this.state.board[ data.x ][ data.y ] === 0) {
+        let move = (client.playerIndex === 0) ? 'x' : 'o'
+        this.state.board[ data.x ][ data.y ] = move
+
+        if (this.checkWin(data.x, data.y, move)) {
+          this.state.winner = client.id
+
+        } else if (this.checkBoardComplete()) {
+          this.state.draw = true
+
+        } else {
+          // switch turn
+          let playerIds = Object.keys(this.state.players)
+            , otherPlayerIndex = (client.playerIndex === 0) ? 1 : 0
+          this.state.currentTurn = playerIds[otherPlayerIndex]
+
+          if (this.randomMoveTimeout) this.randomMoveTimeout.clear()
+          this.randomMoveTimeout = this.clock.setTimeout(this.doRandomMove.bind(this, this.clients[ otherPlayerIndex ]), TURN_TIMEOUT * 1000)
+        }
+
+      }
+    }
+  }
+
+  checkBoardComplete () {
+    return this._flatten(this.state.board).
+      filter(item => item === 0).length === 0
+  }
+
+  doRandomMove (client) {
+    for (let x=0; x<this.state.board.length; x++) {
+      for (let y=0; y<this.state.board[x].length; y++) {
+        if (this.state.board[x][y]===0) {
+          this.onMessage (client, { x: x, y: y })
+          return
+        }
+      }
+    }
+  }
+
+  checkWin (x, y, move) {
+    let won = false
+      , board = this.state.board
+      , boardSize = this.state.board.length
+
+    // horizontal
+    for(let i = 0; i < boardSize; i++){
+      if (board[x][i] !== move) { break; }
+      if (i == boardSize-1) {
+        won = true
+      }
+    }
+
+    // vertical
+    for(let i = 0; i < boardSize; i++){
+      if (board[i][y] !== move) { break; }
+      if (i == boardSize-1) {
+        won = true
+      }
+    }
+
+    // cross forward
+    if(x === y) {
+      for(let i = 0; i < boardSize; i++){
+        if(board[i][i] !== move) { break; }
+        if(i == boardSize-1) {
+          won = true
+        }
+      }
+    }
+
+    // cross backward
+    for(let i = 0; i<boardSize; i++){
+      if(board[i][(boardSize-1)-i] !== move) { break; }
+      if(i == boardSize-1){
+        won = true
+      }
+    }
+
+    return won;
+  }
+
+  onLeave (client) {
+    delete this.state.players[ client.id ]
+
+    if (this.randomMoveTimeout) this.randomMoveTimeout.clear()
+
+    let remainingPlayerIds = Object.keys(this.state.players)
+    if (remainingPlayerIds.length > 0) {
+      this.state.winner = remainingPlayerIds[0]
+    }
+  }
+
+  _flatten(arr, previous) {
+    let flattened = previous || []
+
+    for (let i=0; i<arr.length; i++) {
+      if (arr[i] instanceof Array) {
+        this._flatten(arr[i], flattened)
+
+      } else {
+        flattened.push(arr[i])
+      }
+    }
+
+    return flattened
+  }
+
+}
+
+module.exports = TicTacToe
